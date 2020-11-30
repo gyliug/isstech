7d5da36ed8a7914f9a8d765be22a5e2890d02e07
entfrm <1029861695@qq.com>
2020-11-30 21:48:22 +0800

    entfrm-boot2.0

--- entfrm-biz/entfrm-toolkit/src/main/java/com/entfrm/biz/devtool/service/DatasetService.java
+++
@@ -1,8 +0,0 @@
-package com.entfrm.biz.devtool.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.entfrm.biz.devtool.entity.Dataset;
-
-public interface DatasetService extends IService<Dataset> {
-
-}
